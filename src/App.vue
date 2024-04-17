5f69de1bba148c4db2f64f1c11b63c2d7272c541
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-17 13:45:25 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,33 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from './store.js';
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        store
+      }
+      
+    },
+    methods:{
+      getMovies(){
+        axios.get(this.store.apiUrl + this.store.endPoint.movie, this.store.options).then((res) =>{
+          console.log(res.data.results);
+        })
+  
+      },
+      getSeries(){
+        axios.get(this.store.apiUrl + this.store.endPoint.tv, this.store.options).then((res) => {
+          console.log(res.data.results);
+        })
+      }
+    },
+    created(){
+      this.getMovies();
+      this.getSeries();
+    }
   }
 </script>
 
---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { reactive } from 'vue';
+
+export const store = reactive ({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    endPoint: {
+        movie: 'search/movie',
+        tv: 'search/movie'
+    },
+    options:{
+        params: {
+            api_key: 'fef663df583834922ff3702dcfcbbcb6',
+            query: 'a'
+        }
+    }
+
+});
